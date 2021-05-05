5ffe9a77903b5216e99762b70bb5804a047f7c8b
Arjit Jain <thearjitjain@gmail.com>
2021-05-05 17:48:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
+# nvcc -o coin coin.cu -lcublas
+# python preprocessing.py TORCH_MODEL_FILE
+# ./coin NUM_LAYERS DIM HEIGHT WIDTH RESX RESY STARTX STARTY ENDX ENDY > OUT
+# python postprocessing.py OUT RESX RESY
+
 nvcc -o coin coin.cu -lcublas
-python preprocessing.py TORCH_MODEL_FILE
-./coin NUM_LAYERS DIM HEIGHT WIDTH RESX RESY STARTX STARTY ENDX ENDY > OUT
-python postprocessing.py OUT RESX RESY
+python preprocessing.py best_model_3.pt
+./coin 5 20 512 768 512 768 0 0 511 767 > OUT
+python postprocessing.py OUT 512 768
