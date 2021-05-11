213f6207fdad6d97d0dc6856252676d8157037cb
Arjit Jain <thearjitjain@gmail.com>
2021-05-11 19:07:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # ./coin NUM_LAYERS DIM HEIGHT WIDTH RESX RESY STARTX STARTY ENDX ENDY > OUT
 # python postprocessing.py OUT RESX RESY
 
-nvcc -o coin coin.cu -lcublas
+nvcc -o coin $1 -lcublas
 python preprocessing.py best_model_3.pt
 ./coin 5 20 512 768 512 768 0 0 511 767 > OUT
 python postprocessing.py OUT 512 768
