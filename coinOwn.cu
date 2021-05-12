0ece93eea6ca922809ecccd4ddc98ca526451056
Arjit Jain <thearjitjain@gmail.com>
2021-05-12 13:16:40 +0530

    Update coinOwn.cu

--- coinOwn.cu
+++ coinOwn.cu
@@ -180,7 +180,8 @@ int main(int argc, char* argv[]){
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
-
+    NUM_BLOCKS=ceil((float)(COORDS*DIM)/NUM_THREADS);
+        
     for(int layer=0;layer<NUM_LAYERS;layer++){
         string weightsfileName = "weightsT/net."+to_string(layer)+".linear.weight";
         string biasfileName = "weightsT/net."+to_string(layer)+".linear.bias";
@@ -206,7 +207,6 @@ int main(int argc, char* argv[]){
             MatrixMultiply<<<blocks,threads>>>(COORDS, DIM, DIM, X, DIM, W, DIM, Z, DIM);
         }
         cudaDeviceSynchronize();
-        NUM_BLOCKS=ceil((float)(COORDS*DIM)/NUM_THREADS);
         sineActivation<<<NUM_BLOCKS, NUM_THREADS>>>(X, Z, COORDS*DIM);
         cudaDeviceSynchronize();
     }
