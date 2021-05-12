95d6a9eb75131d68e83828ac459e18d1dbfe62c7
Arjit Jain <thearjitjain@gmail.com>
2021-05-12 13:15:58 +0530

    Update coinOwnUnifiedHints.cu

--- coinOwnUnifiedHints.cu
+++ coinOwnUnifiedHints.cu
@@ -184,7 +184,8 @@ int main(int argc, char* argv[]){
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
-
+    NUM_BLOCKS=ceil((float)(COORDS*DIM)/NUM_THREADS);
+        
     for(int layer=0;layer<NUM_LAYERS;layer++){
         string weightsfileName = "weightsT/net."+to_string(layer)+".linear.weight";
         string biasfileName = "weightsT/net."+to_string(layer)+".linear.bias";
@@ -213,7 +214,6 @@ int main(int argc, char* argv[]){
         }
         cudaDeviceSynchronize();
       
-        NUM_BLOCKS=ceil((float)(COORDS*DIM)/NUM_THREADS);
         sineActivation<<<NUM_BLOCKS, NUM_THREADS>>>(X, Z, COORDS*DIM);
         cudaDeviceSynchronize();
         cudaMemPrefetchAsync(W, weightSize*sizeof(float), cudaCpuDeviceId);
