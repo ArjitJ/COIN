218b3acd78e40d7d4dc832ef49861d5bdc22738d
Arjit Jain <thearjitjain@gmail.com>
2021-05-12 10:37:20 +0530

    Update coinUnifiedHints.cu

--- coinUnifiedHints.cu
+++ coinUnifiedHints.cu
@@ -34,6 +34,16 @@ void readIntoArray(float* arr, ifstream* inFile, int SIZE){
         inFile->close();
     }
 }
+__global__ void fillCoordinateMatrixCUDA(float* X, float start_x, float start_y, float diff_x, float diff_y, int RESX, int RESY){
+    int idx;
+    int tidx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    int tidy = (blockIdx.y * blockDim.y) + threadIdx.y;
+    if(tidx < RESX && tidy < RESY){
+        idx = 2*(tidx*RESY + tidy);
+        X[idx++] = start_x + tidx*(diff_x);
+        X[idx++] = start_y + tidy*(diff_y);
+    }
+}
 void fillCoordinateMatrix(float* X, int STARTX, int STARTY, int ENDX, int ENDY, int RESX, int RESY, int HEIGHT, int WIDTH){
     float start_x = STARTX/(HEIGHT-1.0);
     start_x -= 0.5;
@@ -55,24 +65,34 @@ void fillCoordinateMatrix(float* X, int STARTX, int STARTY, int ENDX, int ENDY, 
         tmp = start_y;
     }
 }
+
 int main(int argc, char* argv[]){
 
     int INP_DIM = 2;
     int OUT_DIM = 3;
 
     // ArgParse
-    int NUM_LAYERS, DIM, HEIGHT, WIDTH, RESX, RESY, STARTX, STARTY, ENDX, ENDY;
+    int NUM_LAYERS, DIM, HEIGHT, RESX, RESY, STARTX, STARTY, ENDX, ENDY;
     NUM_LAYERS = atoi(argv[1]);
     DIM = atoi(argv[2]);
     HEIGHT = atoi(argv[3]);
-    WIDTH = atoi(argv[4]);
+//    WIDTH = atoi(argv[4]);
     RESX = atoi(argv[5]);
     RESY = atoi(argv[6]);
     STARTX = atoi(argv[7]);
     STARTY = atoi(argv[8]);
     ENDX = atoi(argv[9]);
     ENDY = atoi(argv[10]);
+    float start_x = STARTX/(HEIGHT-1.0);
+    start_x -= 0.5;
+    start_x *= 2.0;
+    float start_y = STARTY/(HEIGHT-1.0);
+    start_y -= 0.5;
+    start_y *= 2.0;
+    float diff_x = 2*((ENDX-STARTX)/(HEIGHT-1.0))/RESX;
+    float diff_y = 2*((ENDY-STARTY)/(HEIGHT-1.0))/RESY;
     
+
     ifstream inFile;
 	float* W;
 	float* B;
@@ -104,23 +124,26 @@ int main(int argc, char* argv[]){
     cudaMallocManaged(&B, biasSize*sizeof(float));
     cudaMallocManaged(&X, COORDS*DIM*sizeof(float));
     
-    cudaMemPrefetchAsync(Z, outputSize*sizeof(float), cudaCpuDeviceId);
-//	cudaMemAdvise(W, weightSize*sizeof(float), cudaMemAdviseSetPreferredLocation, id);
-    cudaMemAdvise(B, biasSize*sizeof(float), cudaMemAdviseSetPreferredLocation, cudaCpuDeviceId);   
-//	cudaMemAdvise(X, COORDS*DIM*sizeof(float), cudaMemAdviseSetPreferredLocation, cudaCpuDeviceId);   
-// 	cudaMemPrefetchAsync(Z, outputSize*sizeof(float), id);
-    cudaMemAdvise(W, weightSize*sizeof(float), cudaMemAdviseSetReadMostly, id);
-
-    fillCoordinateMatrix(X, STARTX, STARTY, ENDX, ENDY, RESX, RESY, HEIGHT, WIDTH);
-	cudaMemPrefetchAsync(X, COORDS*DIM*sizeof(float), id);
     cudaMemAdvise(X, COORDS*DIM*sizeof(float), cudaMemAdviseSetPreferredLocation, id);   
-	cudaEventCreate(&start);
+    cudaMemAdvise(Z, outputSize*sizeof(float), cudaMemAdviseSetPreferredLocation, id);   
+ 	cudaMemPrefetchAsync(X, COORDS*DIM*sizeof(float), id);
+    cudaMemPrefetchAsync(Z, outputSize*sizeof(float), id);
+
+    dim3 threads(32, 32);
+    dim3 blocks(ceil((float)RESX/32), ceil((float)RESY/32));
+    fillCoordinateMatrixCUDA<<<blocks, threads>>>(X, start_x, start_y, diff_x, diff_y, RESX, RESY);
+    cudaDeviceSynchronize();
+
+    cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 	cudaEventRecord(start, 0);
     NUM_BLOCKS=ceil((float)(COORDS*DIM)/NUM_THREADS);
     for(int layer=0;layer<NUM_LAYERS;layer++){
         string weightsfileName = "weightsT/net."+to_string(layer)+".linear.weight";
         string biasfileName = "weightsT/net."+to_string(layer)+".linear.bias";
+        inFile.open(biasfileName.c_str());
+        readIntoArray(B, &inFile, biasSize);
+        cudaMemPrefetchAsync(B, biasSize*sizeof(float), id);
         inFile.open(weightsfileName.c_str());
         if(layer == 0){
             readIntoArray(W, &inFile, DIM*INP_DIM);
@@ -129,20 +152,8 @@ int main(int argc, char* argv[]){
             readIntoArray(W, &inFile, weightSize);
         }
         cudaMemPrefetchAsync(W, weightSize*sizeof(float), id);
-  
-        inFile.open(biasfileName.c_str());
-        readIntoArray(B, &inFile, biasSize);
-        idx=0;
-        /*
-        for(int j=0;j<COORDS;j++){
-            for(int i=0;i<biasSize;i++){
-        		Z[idx++] = B[i];
-        	}
-		}
-        */
         copyBias<<<NUM_BLOCKS, NUM_THREADS>>>(Z, B, COORDS*biasSize, biasSize);
         cudaDeviceSynchronize();
-        cudaMemPrefetchAsync(Z, outputSize*sizeof(float), id);
         if(layer == 0){
             cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, DIM, COORDS, INP_DIM, &alpha, W, DIM, X, INP_DIM,
                     &beta, Z, DIM);
@@ -154,27 +165,21 @@ int main(int argc, char* argv[]){
         cudaDeviceSynchronize();
         sineActivation<<<NUM_BLOCKS, NUM_THREADS>>>(X, Z, COORDS*DIM);
         cudaDeviceSynchronize();
-        cudaMemPrefetchAsync(Z, outputSize*sizeof(float), cudaCpuDeviceId);
         cudaMemPrefetchAsync(W, weightSize*sizeof(float), cudaCpuDeviceId);
+        cudaMemPrefetchAsync(B, biasSize*sizeof(float), cudaCpuDeviceId);
     }
-    cudaMemAdvise(X, COORDS*DIM*sizeof(float), cudaMemAdviseSetReadMostly, id);
-    
     string weightsfileName = "weightsT/last_layer.linear.weight";
     string biasfileName = "weightsT/last_layer.linear.bias";
+    inFile.open(biasfileName.c_str());
+    readIntoArray(B, &inFile, OUT_DIM);
+    cudaMemPrefetchAsync(B, biasSize*sizeof(float), id);
+
     inFile.open(weightsfileName.c_str());
     readIntoArray(W, &inFile, DIM*OUT_DIM);
     cudaMemPrefetchAsync(W, weightSize*sizeof(float), id);
 
-    inFile.open(biasfileName.c_str());
-    readIntoArray(B, &inFile, OUT_DIM);
-    idx=0;
-
-    for(int j=0;j<COORDS;j++){
-        for(int i=0;i<biasSize;i++){
-            Z[idx++] = B[i];
-        }
-    }
-    cudaMemPrefetchAsync(Z, outputSize*sizeof(float), id);
+    copyBias<<<NUM_BLOCKS, NUM_THREADS>>>(Z, B, COORDS*biasSize, biasSize);
+    cudaDeviceSynchronize();
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, OUT_DIM, COORDS, DIM, &alpha, W, OUT_DIM, X, DIM,
             &beta, Z, OUT_DIM);
     cudaDeviceSynchronize();
